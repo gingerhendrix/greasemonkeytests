3191f99baee3e4766b36eef1582c530c7da4c7fe
ga <ga@5d58421e-2428-0410-a994-821f96727b03>
2008-01-04 01:13:24 +0000

    Added a few more edge cases to get/set_value tests
    git-svn-id: svn+ssh://s1439.gridserver.com/home/1439/users/.home/repos/GreasemonkeyTests/trunk@413 5d58421e-2428-0410-a994-821f96727b03

--- tests/functional/GetSetValueTest.user.js
+++ tests/functional/GetSetValueTest.user.js
@@ -52,6 +52,27 @@ new TestSuite("setValue Tests", {
     t.assert(typeof(val) == "number", "Unexpected type " + typeof(val) + "");
   },
   
+  testNegativeInteger : function(t){
+    GM_setValue("testNegativeInteger", -4);
+    var val = GM_getValue("testNegativeInteger");
+    t.assert(val === -4, "Unexpected value " + val);
+    t.assert(typeof(val) == "number", "Unexpected type " + typeof(val) + "");
+  },
+  
+  testMaxInteger : function(t){
+    GM_setValue("testMaxInteger", Math.pow(2,31)-1);
+    var val = GM_getValue("testMaxInteger");
+    t.assert(val === Math.pow(2,31)-1, "Unexpected value " + val);
+    t.assert(typeof(val) == "number", "Unexpected type " + typeof(val) + "");
+  },
+  
+  testMaxNegativeInteger : function(t){
+    GM_setValue("testMaxNegativeInteger", -(Math.pow(2,31)-1));
+    var val = GM_getValue("testMaxNegativeInteger");
+    t.assert(val === -(Math.pow(2,31)-1), "Unexpected value " + val);
+    t.assert(typeof(val) == "number", "Unexpected type " + typeof(val) + "");
+  },
+  
   testBooleanTrue : function(t){
     GM_setValue("testBooleanTrue", true);
     var val = GM_getValue("testBooleanTrue");
@@ -66,10 +87,29 @@ new TestSuite("setValue Tests", {
     t.assert(typeof(val) === "boolean", "Unexpected type " + typeof(val) + "");
   },
   
+  testTypeChange : function(t){
+    GM_setValue("testTypeChange", false);
+    var val = GM_getValue("testTypeChange");
+    t.assert(val === false, "Unexpected value " + val);
+    t.assert(typeof(val) === "boolean", "Unexpected type " + typeof(val) + "");
+    GM_setValue("testTypeChange", "test");
+    val = GM_getValue("testTypeChange");
+    t.assert(val === "test", "Unexpected value " + val);
+    t.assert(typeof(val) == "string", "Unexpected type " + typeof(val) + "");
+    t.assert(val.constructor == String, "Unexpected constructor " + val.constructor);
+  }
+  
 });
 
 new TestSuite("Persistence Test", {
-  //TODO: Not sure how to test persistence?
+  testPersistence : function(t){
+    var val = GM_getValue("testPersistence", 0);
+    GM_setValue("testPersistence", val+1);
+    if(val==0){
+      t.fail("This is the first time the test has been run, run the test again to make this test pass")
+    }
+    t.log("This test has been run " + (val+1) + " times")
+  }
 });
 
 new TestSuite("setValue error tests", {
@@ -106,6 +146,20 @@ new TestSuite("setValue error tests", {
     }catch(e){
       t.assert(true, "An error should be thrown") 
     }
+  },
+  
+  testIntegerOverflow : function(t){
+    GM_setValue("testIntegerOverflow", Math.pow(2,32));
+    var val = GM_getValue("testIntegerOverflow");
+    t.assert(val !== Math.pow(2,32), "Unexpected assertion success")
+    t.assert(val === 0, "Unexpected value " + val);
+  },
+  
+  testDateOverflow : function(t){
+    var d = new Date();
+    GM_setValue("testDateOverflow", Number(d));
+    var val = GM_getValue("testDateOverflow");
+    t.assert(val !== Number(d), "Unexpected assertion success")
   }
   
 });
